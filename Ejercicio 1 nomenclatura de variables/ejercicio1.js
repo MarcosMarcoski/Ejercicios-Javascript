b28d649751898e300df1d579a163869d15b04603
Marcoski <marcosdediego@gmail.com>
2023-07-19 12:55:35 +0200

    Ejercicio 1

---
+++ Ejercicio 1 nomenclatura de variables/ejercicio1.js
@@ -0,0 +1,11 @@
+
+// Ejercicio 1 
+
+let brand = "Toyota";
+let model = "Camry";
+let productionYear = 2022;
+let ownerName = "John Doe";
+
+const isNew = true;
+
+// las variables let pueden variar, mientras que las de tipo const no.
