2f5fedcbd541a02e561b32778dd401a377c46ab4
Marcoski <marcosdediego@gmail.com>
2023-07-19 19:11:06 +0200

    ejercicio 11

---
+++ Ejercicio 11 comparacion de variables/ejercicio 11.js
@@ -0,0 +1,22 @@
+
+// Ejercicio 11
+
+10 > 9;
+45 > 34;
+true == 1;
+false == 0;
+1 > 0;
+4 > 2;
+4 < 8;
+
+// Imprimir en consola
+
+console.log("Resultado:", 10 > 9);
+console.log("Resultado:", 45 > 34);
+console.log("Resultado:", true == 1);
+console.log("Resultado:", false == 0);
+console.log("Resultado:", 1 > 0);
+console.log( "Resultado:", 4 > 2);
+console.log("Resultado:", 4 < 8);
+
+// Todos los Resultados en la consola son true
