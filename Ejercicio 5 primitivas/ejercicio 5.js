f6903916c3866c3fe74e76aae751bf15e2e2b5fe
Marcoski <marcosdediego@gmail.com>
2023-07-19 15:47:20 +0200

    ejercicio 5

---
+++ Ejercicio 5 primitivas/ejercicio 5.js
@@ -0,0 +1,19 @@
+
+
+// Ejercicio 5
+
+let isDoorClosed = false
+let isDogOutside = true
+
+
+// Código del ejercicio
+
+if (isDoorClosed == true && isDogOutside == false) {
+    console.log("the door is closed and the dog is inside");
+    } else if (isDoorClosed == false && isDogOutside == true) {
+    console.log("The door is open and the dog is outside");
+    } else console.log("Modify the variables values");
+
+
+// The door is open and the dog is outside
+// pone justo lo contrario
