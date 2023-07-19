e9d2fba1f35d9154b587d26eb140f4e96644db83
Marcoski <marcosdediego@gmail.com>
2023-07-19 16:06:50 +0200

    ejercicio 6

---
+++ Ejercicio 6 conversion de tipos/ejercicio 6.js
@@ -0,0 +1,15 @@
+
+// Ejercicio 6
+
+let hello = 'Ciao';         
+let age = 18;               
+let isGraduated = false;    
+
+// Convertir hello a tipo de datos booleano
+hello = Boolean(hello);
+
+// Convertir age a tipo de datos cadena (string)
+age = String(age);
+
+// Convertir isGraduated a tipo de datos numérico (number)
+isGraduated = Number(isGraduated);
