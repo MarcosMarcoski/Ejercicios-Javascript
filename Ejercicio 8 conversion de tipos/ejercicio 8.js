17aa53bd2e2e448e403888d713ba204c5be88594
Marcoski <marcosdediego@gmail.com>
2023-07-19 17:36:18 +0200

    Ejercicio 8

---
+++ Ejercicio 8 conversion de tipos/ejercicio 8.js
@@ -0,0 +1,31 @@
+// Ejercicio 8
+
+let ejercicio = true;
+
+// Dato original
+
+console.log("Datos originales", ejercicio);
+console.log("Datos originales", typeof ejercicio);
+
+// Dato number
+
+ejercicio = Number(ejercicio);
+
+console.log("Dato number", ejercicio);
+console.log("Dato number", typeof ejercicio);
+
+// Dato string
+
+ejercicio = String(ejercicio);
+
+console.log("Dato string", ejercicio);
+console.log("Dato string", typeof ejercicio);
+
+// Dato boolean
+
+ejercicio = Boolean(ejercicio);
+
+console.log("Dato boolean", ejercicio);
+console.log("Dato boolean ", typeof ejercicio);
+
+// realmente la última parte es igual que la primera, ya que es un dato Boolean, y con el typeof la consola se puede ver que tipo de dato estamos ejecutando
