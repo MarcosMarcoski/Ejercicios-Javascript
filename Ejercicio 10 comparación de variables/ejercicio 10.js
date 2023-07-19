0846cf4de341863cd92eaf7a0929045742908cc4
Marcoski <marcosdediego@gmail.com>
2023-07-19 18:48:40 +0200

    ejercicio 10

---
+++ Ejercicio 10 comparación de variables/ejercicio 10.js
@@ -0,0 +1,17 @@
+
+// Ejercicio 10
+
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted
+yearsCompleted === firstYearCompleted
+
+
+// == significa que tiene el mismo valor, por eso si lo ejecutamos en la consola saldrá que es verdadero, ya que True es 1 en el lenguaje binario.
+
+console.log(yearsCompleted == firstYearCompleted); //true
+
+// === significa estrictamente igual, por eso si lo ejecutamos en la consola nos dirá que es false, ya que no es literalmente lo mismo.
+
+console.log(yearsCompleted === firstYearCompleted); //false
