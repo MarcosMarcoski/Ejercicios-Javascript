b0b235be0bff15d10ff58eacb23b439823b74ba6
Marcoski <marcosdediego@gmail.com>
2023-07-19 17:07:22 +0200

    Ejercicio 7

---
+++ Ejercicio 7 conversion de tipos/ejercicio 7.js
@@ -0,0 +1,20 @@
+
+// Ejercicio 7
+
+let hello = 'Ciao';     // number
+let age = 18;       // boolean
+let isGraduated = false;     // string
+
+// conversión de tipos
+
+hello = Number(hello)
+age = Boolean(age)
+isGraduated = String(isGraduated)
+
+// Imprimir en consola
+
+console.log(hello)
+console.log(age)
+console.log(isGraduated)
+
+// A la hora de ejecutar en consola la variable hello, esta nos dice NaN, que quiere decir que no es un valor numérico.
