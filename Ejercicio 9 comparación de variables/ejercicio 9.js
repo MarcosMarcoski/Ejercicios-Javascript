aa72706e4703bab02af79367a76404f01060e5b3
Marcoski <marcosdediego@gmail.com>
2023-07-19 18:32:09 +0200

    Ejercicio 9

---
+++ Ejercicio 9 comparación de variables/ejercicio 9.js
@@ -0,0 +1,34 @@
+
+// Ejercicio 9
+
+const namee = "Mario";
+const lastname = "Rossi";
+const age = 27;
+const isGraduated = false;
+const average = 27;
+const examsCompleted = 10;
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+// Operaciones
+
+namee == lastname;
+age <= average;
+firstYearCompleted == lastname;
+yearsCompleted == firstYearCompleted;
+firstYearCompleted === yearsCompleted;
+examsCompleted < age;
+isGraduated > yearsCompleted;
+
+// Imprimir consola
+
+console.log(namee == lastname);//false
+console.log(age <= average); //true
+console.log(firstYearCompleted == lastname); //false
+console.log(yearsCompleted == firstYearCompleted); //true
+console.log(firstYearCompleted === yearsCompleted); //false
+console.log(examsCompleted < age); //true
+console.log(isGraduated > yearsCompleted); //false
+
+
+
