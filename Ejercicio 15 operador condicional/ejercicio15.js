a9f2a79a8bd45938fbeef8fc93e3d6117bb17017
Marcoski <marcosdediego@gmail.com>
2023-07-24 14:30:58 +0200

    ejercicio 15

---
+++ Ejercicio 15 operador condicional/ejercicio15.js
@@ -0,0 +1,10 @@
+
+// Ejercicio 15
+
+
+let age = 20;
+let mensaje = age >= 18 ? "you can drive a car" : "You are too young to drive."; // you can drive a car
+console.log(mensaje);
+
+
+// Si redujeses el numero declarado en la variable age por debajo de 18, en la consola pondría "You are too young to drive."
