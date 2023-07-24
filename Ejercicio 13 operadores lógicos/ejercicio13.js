98376aa4e8c396b7c0906fed24fc85dd88064225
Marcoski <marcosdediego@gmail.com>
2023-07-24 14:05:28 +0200

    ejercicio 13

---
+++ Ejercicio 13 operadores lógicos/ejercicio13.js
@@ -0,0 +1,8 @@
+
+// Ejercico 13
+
+
+console.log("testing" == "testing" && "Mario" == "Cool Guy") //the result must be false
+
+
+// Con "&&" deben ser las dos expresiones verdaderas, de lo contrario saldrá false
