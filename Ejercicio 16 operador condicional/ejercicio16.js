432febbcbe561103131f3cc405722f8b459a6e96
Marcoski <marcosdediego@gmail.com>
2023-07-24 14:44:16 +0200

    ejercicio 16

---
+++ Ejercicio 16 operador condicional/ejercicio16.js
@@ -0,0 +1,10 @@
+
+// Ejercicio 16
+
+
+let isSunnyDay = true;
+let isHomeworkCompleted = true;
+
+let mensaje = isSunnyDay && isHomeworkCompleted ? "Jesse can go out to play" : "Jesse stays home";
+console.log(mensaje);
+
