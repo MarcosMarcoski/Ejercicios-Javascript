e91a2a4d299604ba00b167dc7b65913d4c3bc689
Marcoski <marcosdediego@gmail.com>
2023-07-25 20:56:30 +0200

    ejercicio 23

---
+++ Ejercicio 23 switch/ejercicio 23.js
@@ -0,0 +1,20 @@
+// Ejercicio 23
+
+let precioHab = "junior suite";
+
+switch (precioHab) {
+  case "basic":
+    console.log("Prince for Basic Room --> 50€");
+    break;
+
+  case "junior suite":
+    console.log("Price for Junior Suite Room --> 80€");
+    break;
+
+  case "master suite":
+    console.log("Price for Master Suite  --> 100€");
+    break;
+
+  default:
+    console.log("No se han encontrado otras habitaciones");
+}
