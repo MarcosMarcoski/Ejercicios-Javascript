361b3d41ea122bf1d3acc2eb3446ab620272c143
Marcoski <marcosdediego@gmail.com>
2023-07-25 13:56:04 +0200

    ejercicio 17

---
+++ Ejercicio 17 if/ejercicio17.js
@@ -0,0 +1,12 @@
+// Ejercicio 17
+
+let age = 20;
+let mensaje;
+
+if (age >= 18) {
+  mensaje = "you can drive a car";
+} else {
+  mensaje = "You are too young to drive.";
+}
+
+console.log(mensaje);
