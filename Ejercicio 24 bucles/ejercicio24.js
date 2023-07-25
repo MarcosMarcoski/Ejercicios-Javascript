221a04bc872074789a806b088e20108902c40e73
Marcoski <marcosdediego@gmail.com>
2023-07-25 21:16:47 +0200

    ejercicio 24

---
+++ Ejercicio 24 bucles/ejercicio24.js
@@ -0,0 +1,8 @@
+
+// Ejercicio 24
+
+for (let i = 0; i <= 10; i++) {
+    console.log("Número:" + i);
+  }
+  
+//   Bucle for: Se utiliza cuando sabes exactamente cuántas veces quieres repetir un bloque de código.
