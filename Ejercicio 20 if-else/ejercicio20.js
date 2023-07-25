c15d607c052436a5c5d05e871fd74b70713a6f0c
Marcoski <marcosdediego@gmail.com>
2023-07-25 20:07:05 +0200

    ejercicio 20

---
+++ Ejercicio 20 if-else/ejercicio20.js
@@ -0,0 +1,15 @@
+// Ejercicio 20
+
+// Crear la variable primitive y asignarle un valor
+let primitive = "hola!";
+
+// Comprobar el tipo de la variable y mostrarlo en la consola
+if (typeof primitive === "number") {
+  console.log("La variable primitive es de tipo numérico.");
+} else if (typeof primitive === "string") {
+  console.log("La variable primitive es de tipo string.");
+} else if (typeof primitive === "boolean") {
+  console.log("La variable primitive es de tipo booleano.");
+} else {
+  console.log("La variable primitive tiene un tipo no reconocido.");
+}
