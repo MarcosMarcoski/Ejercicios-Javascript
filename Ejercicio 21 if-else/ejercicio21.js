38d5cbf03449e885a0129be15081e92dad235efc
Marcoski <marcosdediego@gmail.com>
2023-07-25 20:15:38 +0200

    ejercicio 21

---
+++ Ejercicio 21 if-else/ejercicio21.js
@@ -0,0 +1,12 @@
+
+// Ejercicio 21
+
+let password = "12345";
+
+// Comprobar la longitud de la contraseña en la cosola
+
+if (password.length >= 4 && password.length <= 6) {
+  console.log("Password length is correct.");
+} else {
+  console.log("Password not valid.");
+}
