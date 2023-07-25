2cc308bf4a3f803daa23e2ed0cd127ca3a4ac74e
Marcoski <marcosdediego@gmail.com>
2023-07-25 14:19:43 +0200

    ejercicio 19

---
+++ Ejercicio 19 if/ejercicio19.js
@@ -0,0 +1,15 @@
+
+// Ejercicio 19
+
+let namee;
+let surname = 'Rossi';
+
+if (namee && surname) {
+
+    console.log("Nombre completo:" ,namee, surname);
+}
+
+else{
+
+    console.log("Full name is invalid");
+}
