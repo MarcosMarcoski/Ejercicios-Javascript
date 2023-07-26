6a8a6025ece35ba97603422fab85d712b78c94da
Marcoski <marcosdediego@gmail.com>
2023-07-26 02:01:29 +0200

    ejercicio 30

--- Ejercicio 29 funciones/ejercicio 29.js
+++ Ejercicio 29 funciones/ejercicio 29.js
@@ -5,6 +5,6 @@ function printName() {
     console.log("Nombre: Marcoski");
   }
   
-  // Llamada a la función para imprimir el nombre
+  // Llamada a la función para que imprima en la consola
   printName();
   
---
+++ Ejercicio 30 funciones/ejercicio 30.js
@@ -0,0 +1,12 @@
+
+// Ejercicio 30
+
+let Namee= "Marcos"
+
+function sayHelloName () {
+
+    console.log("Hola", Namee, "!!!")
+    
+}
+
+sayHelloName();
