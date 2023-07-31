5cc330b954ba5a98e9a671d3ac4cbe2795d5af23
Marcoski <marcosdediego@gmail.com>
2023-07-31 12:13:26 +0200

    ejercicio 35

---
+++ Ejercicio 35 funciones flecha/ejercicio35.js
@@ -0,0 +1,24 @@
+
+// Ejercicio 35
+
+function canPlay() {
+  let sport = " Football";
+
+  if (true) {
+    let personName = "Cosimo";
+    console.log(personName + sport);
+  }
+
+  
+}
+
+canPlay();
+
+// Al no tener el console.log dentro del bloque de if, no reconocía bien la funcion personName
+
+
+  
+
+
+
+  
