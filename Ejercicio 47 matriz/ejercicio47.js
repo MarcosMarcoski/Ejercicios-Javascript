7716d49cb3037bd3700c09ed0b7164ce3511dbae
Marcoski <marcosdediego@gmail.com>
2023-07-31 15:19:18 +0200

    ejercicio 47

---
+++ Ejercicio 47 matriz/ejercicio47.js
@@ -0,0 +1,10 @@
+
+// Ejercicio 47
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0])
+const lastindex = numbers.length -1;
+console.log (numbers[lastindex])
+console.log(numbers.length)
+console.log(numbers[19]) //undefined ya que no hay tantos elementos en la matriz
