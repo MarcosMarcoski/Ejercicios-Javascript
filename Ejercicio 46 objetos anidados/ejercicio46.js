881a0d641dbc235cdc55d2035465fbedd576874d
Marcoski <marcosdediego@gmail.com>
2023-07-31 14:54:51 +0200

    ejercicio 46

---
+++ Ejercicio 46 objetos anidados/ejercicio46.js
@@ -0,0 +1,19 @@
+
+// Ejercicio 46
+
+
+const student = {
+    id: 1,
+    school: "Liceo",
+    year: 3,
+  }
+
+student.personalData = {
+
+ name: "Marcos",
+ surname: "de Diego",
+ age:30
+ 
+
+  }
+  console.log(student.personalData)
