99cabe3740577cef457b29cc86506b8ac31c21d7
Marcoski <marcosdediego@gmail.com>
2023-07-31 14:35:48 +0200

    ejercicio 45

---
+++ Ejercicio 45 funciones constructoras/ejercicio45.js
@@ -0,0 +1,13 @@
+// Ejercicio 45
+
+function smarthphone(brand, name, price) {
+  this.brand = brand;
+  this.name = name;
+  this.priceprice = price;
+}
+
+const smartphone1 = new smarthphone ("samsung","galaxy", 800)
+const smartphone2 = new smarthphone ("apple", "Iphone X", 990)
+
+console.log(smartphone1)
+console.log(smartphone2)
